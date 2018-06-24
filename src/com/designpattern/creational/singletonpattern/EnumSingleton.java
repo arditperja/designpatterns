78e82797f84e01b44ed701bd27008181d88d76a7
arditperja <arditperja@live.com>
2018-06-24 19:47:59 +0200

    other methods of singleton designe pattern implementation

---
+++ src/com/designpattern/creational/singletonpattern/EnumSingleton.java
@@ -0,0 +1,11 @@
+package com.designpattern.creational.singletonpattern;
+
+// Java ensures that any enum value is instantiated only once in a Java program.
+public enum EnumSingleton {
+
+    INSTANCE;
+
+    public static void doSomething(){
+        //do something
+    }
+}
